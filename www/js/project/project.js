ea1c27b1eb7d6a5b750b09f5164cc656b415fb8d
Antonio <antonio.l.puleo@gmail.com>
2016-01-26 10:21:10 +0000

    more amendments to saving and retrieving answers. checkboxes remaining objects, removed answer-service class

--- www/js/project/project.js
+++ www/js/project/project.js
@@ -1,7 +1,8 @@
-angular.module('project', [])
 
-    .controller('ProjectCtrl', function ($ionicPlatform, $scope, $stateParams, $state, WebService, ProjectModel, DbService) {
 
+angular.module('entries', [])
+
+    .controller('EntriesCtrl', function($ionicPlatform, $scope, $stateParams, $state, ProjectModel, DbService, EntryService) {
 
         $scope.show = function() {
             cordova.plugin.pDialog.init({
@@ -9,7 +10,7 @@ angular.module('project', [])
                 progressStyle : 'SPINNER',
                 cancelable : false,
                 title : 'Please Wait...',
-                message : 'Loading Project...'
+                message : 'Loading Entries...'
             });
         };
 
@@ -17,41 +18,94 @@ angular.module('project', [])
             cordova.plugin.pDialog.dismiss();
         };
 
-        $scope.projectRef = $stateParams.project_ref;
-
+        $scope.entries = [];
+        $scope.DbService = DbService;
         $scope.project = ProjectModel;
+        $scope.projectRef = $stateParams.project_ref;
+        $scope.projectName = '';
 
-        $scope.getProject = function() {
+        /**
+         * Retrieve the project and entries
+         */
+        $scope.getProjectAndEntries = function() {
+
             DbService.getProject($scope.projectRef).then(function(res) {
+
+                console.log(res);
 
                 // check if we have one
                 if (res.rows.item(0)) {
 
-                    var data = res.rows.item(0).data;
-                    var extra = res.rows.item(0).extra;
-                    $scope.data = data;
-                    $scope.extra = extra;
-
-                    $scope.project.initialise($scope.data, $scope.extra);
-
+                    $scope.json_structure = res.rows.item(0).json_structure
+                    $scope.json_extra = res.rows.item(0).json_extra;
+                    $scope.project.initialise($scope.json_structure, $scope.json_extra);
+                    EntryService.addProject($scope.project);
                     $scope.projectName = $scope.project.getProjectName();
 
+                    console.log($scope.project);
+                    // now retrieve and show the entries
+                    $scope.showEntries();
+                } else {
+                    $scope.hide();
                 }
-                $scope.hide();
+
 
             });
         };
 
+        /**
+         * Show the entries for this project's main form
+         */
+        $scope.showEntries = function() {
 
+            $scope.DbService.getEntriesForProject($scope.projectRef).then(function(res) {
+
+                for (var i = 0; i < res.rows.length; i++) {
+                    $scope.entries.push({title: res.rows.item(i).uuid});
+                }
+
+                $scope.$apply();
+                $scope.hide();
+
+            });
+
+        };
+
+        /**
+         * View an entry
+         */
+        $scope.viewEntry = function(entry) {
+
+            console.log(entry);
+            $state.go('app.view-entry', {project_ref: $scope.projectRef, entryUuid: entry.title});
+
+        };
+
+        /**
+         * Start the process of adding an entry
+         * Initialise the main entry variables
+         * Add the first question
+         */
+        $scope.addEntry = function() {
+
+            EntryService.setUp();
+            $state.go('app.question', {project_ref: $scope.projectRef, formRef: EntryService.currentFormRef, inputRef: '', inputIndex: 0});
+
+        };
+
+        /**
+         * Go to Projects page
+         */
         $scope.projectsPage = function () {
             location.href = '#/app/projects';
         };
 
         $ionicPlatform.ready(function () {
+
             $scope.show();
-            $scope.getProject();
+            // retrieve the project then the entries
+            $scope.getProjectAndEntries();
+
         });
 
     });
-
-
---
+++ www/js/services/branch-entry-service.js
@@ -0,0 +1,242 @@
+/**
+ * Entry Service
+ */
+angular.module('entry-service', ['ionic'])
+    .service('EntryService', function ($ionicPlatform, StatusCodes, DbService, AnswerValidate) {
+
+        /**
+         * Helper method to create a uuid
+         *
+         * @returns {string}
+         */
+        this.uuid = function () {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                    .toString(16)
+                    .substring(1);
+            }
+
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                s4() + '-' + s4() + s4() + s4();
+        };
+
+        this.entryUuid = '';
+        this.project = '';
+        this.forms = [];
+        this.formName = '';
+        this.numForms = 0;
+        this.currentFormIndex = 0;
+        this.numInputsThisForm = 0;
+        this.inputs = {};
+        this.answers = {};
+        this.currentFormRef = '';
+        this.branchEntries = [];
+        this.media = [];
+        this.validator = AnswerValidate;
+
+        /**
+         * Add project details to the add entry object
+         *
+         * @param project
+         */
+        this.addProject = function (project) {
+            this.project = project;
+        };
+
+        /**
+         * Initial function to set up the entry
+         */
+        this.setUp = function () {
+
+            // reset answers
+            this.answers = {};
+
+            // reset uuid
+            this.entryUuid = this.uuid();
+
+            // get forms
+            var forms = this.project.getExtraForms();
+            this.numForms = forms.length;
+            var inputs;
+
+            var i = 0;
+            // loop forms to get first
+            for (var form in forms) {
+
+                if (forms.hasOwnProperty(form)) {
+                    this.forms[i] = {ref: form, form: forms[form]};
+
+                    // set some class variables so we know our current form/input
+                    // if we don't have a form, we'll use the first form
+                    if (i === 0 && !this.currentFormRef) {
+                        this.formName = forms[form].details.name;
+                        this.currentFormRef = form;
+                        this.currentFormIndex = form;
+                        this.inputs = this.project.getFormInputs(form);
+                        this.numInputsThisForm = this.inputs.length;
+                    }
+
+                    i++;
+                }
+            }
+
+            var inputsExtra = this.project.getExtraInputs();
+
+            // now loop round and prepopulate the answers for each question
+            for (var i = 0; i < this.inputs.length; i++) {
+
+                var inputDetails = inputsExtra[this.inputs[i]].data;
+
+                switch(inputDetails.type) {
+
+                    case 'group':
+
+                        // add group answers to main group
+                        var group = this.project.getFormGroups(this.currentFormRef);
+
+                        this.answers[inputDetails.ref] = {answer: [], input_ref: inputDetails.ref, was_jumped: false};
+                        for (var j = 0; j < group[inputDetails.ref].length; j++) {
+
+                            var groupInputDetails = inputsExtra[group[inputDetails.ref][j]].data;
+                            switch(groupInputDetails.type) {
+
+                                case 'checkbox':
+                                    this.answers[inputDetails.ref].answer.push({answer: {}, input_ref: groupInputDetails.ref, was_jumped: false});
+                                    break;
+                                default:
+                                    this.answers[inputDetails.ref].answer.push({answer: '', input_ref: groupInputDetails.ref, was_jumped: false});
+                            }
+                        }
+
+                        break;
+
+                    case 'checkbox':
+                        this.answers[inputDetails.ref] = {answer: {}, input_ref: inputDetails.ref, was_jumped: false};
+                        break;
+
+                    default:
+                        // add any other type as normal
+                        this.answers[inputDetails.ref] = {answer: '', input_ref: inputDetails.ref, was_jumped: false};
+
+                }
+
+            }
+
+        };
+
+        /**
+         * Make the json Entry object
+         *
+         * @param answers
+         * @returns {{data: {type: string, id: (string|*), entry_uuid: (string|*), attributes: {form: {ref: (string|*), type: string}}, relationships: {parent: {}, branch: {}}, created_at: string, device_id: string, jwt: null}}}
+         */
+        this.makeJsonEntry = function (answers) {
+
+            var entryType = 'entry';
+            var uuid = this.entryUuid;
+            var branch = {};
+            var parent = {};
+
+            var entryJson = {
+                data: {
+                    type: 'entry',
+                    id: uuid,
+                    entry_uuid: uuid,
+                    attributes: {
+                        form: {
+                            ref: this.currentFormRef,
+                            type: 'hierarchy'
+                        }
+                    },
+                    relationships: {
+                        parent: parent,
+                        branch: branch
+                    },
+                    created_at: '2015-11-30T11:07:26+00:00',
+                    device_id: 'a2439aad10124340',
+                    jwt: null
+                }
+
+            };
+
+            entryJson.data[entryType] = answers;
+
+            return entryJson;
+
+        };
+
+        /**
+         * Save an entry
+         *
+         * @param projectRef
+         */
+        this.saveEntry = function(projectRef) {
+
+            var self = this;
+
+            return new Promise(function (fulfill, reject) {
+
+                // Make entry json from the answers
+                var entry = self.makeJsonEntry(self.answers);
+                // Save the entry in the database
+                DbService.saveEntry(projectRef, self.entryUuid, entry).then(function (res) {
+
+                    console.log(entry);
+                    alert(StatusCodes.getMessage('ec5_79'));
+                    fulfill(res);
+
+                }, function (err) {
+                    console.error(err);
+                    reject(err);
+                });
+
+
+            });
+
+
+        };
+
+        /**
+         * Validate and add answer to 'answers' object
+         *
+         * @param answer
+         * @param inputDetails
+         */
+        this.addAnswer = function(answer, inputDetails) {
+
+            // Reset validator errors
+            this.validator.reset();
+
+            // Validate the answer
+            this.validator.validate(inputDetails, answer.answer);
+
+            // Check whether answer is valid
+            if (this.validator.hasErrors()) {
+                var errors = this.validator.getErrors();
+                for (var error in errors) {
+                    if (errors.hasOwnProperty(error)) {
+                        alert('Error: ' + StatusCodes.getMessage(errors[error]));
+                    }
+                }
+                return false;
+            }
+
+            // store the answer
+            this.answers[inputDetails.ref] = answer;
+
+            // success
+            return true;
+
+        };
+
+        /**
+         * Return answer for a question
+         *
+         * @param inputRef
+         * @returns {*}
+         */
+        this.getAnswer = function(inputRef) {
+            return this.answers[inputRef];
+        };
+
+    });
